30af6026868c58db12a50e8d239d4e0334505a81
Matthew Foster <matthew.foster@hakai.org>
2023-05-29 14:12:02 -0700

    change projects type yo stringCollection remove unused solr config correctly populate projects in the solr index update solr8 config

--- contrib/docker/update_solr.sh
+++ contrib/docker/update_solr.sh
@@ -1,4 +1,5 @@
+sudo docker-compose pull solr
 sudo docker-compose rm -f -s solr
-sudo docker volume rm docker_solr
-sudo docker-compose up -d solr
+sudo docker volume rm docker_solr_data
+sudo docker-compose up -d
 sudo docker exec -it ckan ckan --config=/etc/ckan/production.ini search-index rebuild -r
