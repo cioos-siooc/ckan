9ad97a6ee7a4585a4049b908fbe482f45f6bb950
Matthew Foster <matthew.foster@hakai.org>
2023-06-22 11:56:05 -0700

    add a few fields to the solr index so we can include them in field list (fl) queries

--- contrib/docker/update_solr.sh
+++ contrib/docker/update_solr.sh
@@ -1,5 +1,9 @@
-sudo docker-compose pull solr
+# if updating the schema you must build the image first. once the container starts there 
+# is no way to update the managed schema other then via thesolr api
+# sudo docker-compose pull solr OR sudo docker-compose build solr
 sudo docker-compose rm -f -s solr
 sudo docker volume rm docker_solr_data
 sudo docker-compose up -d
+sudo docker-compose restart solr
+sleep 5
 sudo docker exec -it ckan ckan --config=/etc/ckan/production.ini search-index rebuild -r
