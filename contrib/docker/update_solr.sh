2fcb0f2e96586d8ea16eca5dac6ab642c32f7df1
Matthew Foster <matthew.foster@hakai.org>
2023-06-29 19:42:51 -0700

    update solr schema and entrypoint files

--- contrib/docker/update_solr.sh
+++ contrib/docker/update_solr.sh
@@ -4,6 +4,5 @@
 sudo docker-compose rm -f -s solr
 sudo docker volume rm docker_solr_data
 sudo docker-compose up -d
-sudo docker-compose restart solr
 sleep 5
 sudo docker exec -it ckan ckan --config=/etc/ckan/production.ini search-index rebuild -r
